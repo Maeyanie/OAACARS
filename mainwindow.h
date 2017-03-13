da5ceb0a3aeaecb9207799f799ef8d89df881a5a
Maeyanie <me@maeyanie.com>
2017-03-12 21:01:15 -0400

    Add "crashed" detection.

--- mainwindow.h
+++ mainwindow.h
@@ -25,7 +25,7 @@ struct Status {
     float pitch, bank, heading, vs, ias, gs, lat, lon, asl, agl, g;
     float flaps, zfw, fuel, distance, completed, remaining;
     char engine[8];
-    bool gear;
+    bool gear, onRwy;
     bool bea, nav, ldn, str, txi;
     float winddeg, windknots, oat;
 };
@@ -87,6 +87,7 @@ private:
     void taxiSpeed(float speed);
     void qnhTakeoff();
     void qnhLanding();
+    void crashed(QString reason);
 
     Ui::MainWindow *ui;
     QLabel *sRealTime, *sFlightTime;
