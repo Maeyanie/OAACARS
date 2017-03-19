cfed1c17a6832c946ac34ec6d84960b8e684c5c2
Maeyanie <me@maeyanie.com>
2017-03-19 09:22:50 -0400

    Change airports download URL, add option to "airport not found" dialogues to re-download list.

--- airports.cpp
+++ airports.cpp
@@ -34,45 +34,7 @@ Airports::Airports(QObject *parent) : QObject(parent)
         dataFile.close();
     } else {
         qWarning("Could not open 'airports.csv'");
-        curl_global_init(CURL_GLOBAL_ALL);
-        CURL* curl = curl_easy_init();
-        QByteArray curlData;
-        curl_easy_setopt(curl, CURLOPT_URL, "http://www.maeyanie.com/oaacars/airports.csv");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &curlData);
-
-        qint32 ret = curl_easy_perform(curl);
-        long code;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &code);
-        qInfo("Fetching airports.csv, returned: %d\n", ret);
-        if (ret != CURLE_OK || code >= 400) {
-            QMessageBox::critical(NULL, "Error loading airports",
-                                  "Could not open or download airports.csv!",
-                                  QMessageBox::Ok, QMessageBox::NoButton);
-            exit(1);
-        }
-        curl_easy_cleanup(curl);
-
-        if (dataFile.open(QFile::WriteOnly | QFile::Truncate)) {
-            dataFile.write(curlData);
-            dataFile.close();
-        }
-
-        QTextStream in(curlData);
-        QString line, icao;
-        double lat, lon;
-        QStringList parts;
-        int count = 0;
-        while (in.readLineInto(&line)) {
-            parts = line.trimmed().split(',');
-            icao = parts.takeFirst();
-            lat = parts.takeFirst().toDouble();
-            lon = parts.takeFirst().toDouble();
-            //qDebug("[Airports] Loaded %s: %f,%f", icao.toUtf8().data(), lat, lon);
-            airports.insert(icao, QPair<double,double>(lat,lon));
-            count++;
-        }
-        qInfo("Loaded %d airports from HTTP", count);
+        refresh();
     }
 }
 
@@ -80,4 +42,47 @@ QPair<double,double>* Airports::get(QString icao) {
     QMap<QString, QPair<double,double> >::iterator i = airports.find(icao);
     if (i == airports.end()) return NULL;
     return &*i;
+}
+
+void Airports::refresh() {
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL* curl = curl_easy_init();
+    QByteArray curlData;
+    curl_easy_setopt(curl, CURLOPT_URL, "http://openair-alliance.eu/vaManager/vam/airport_csv.php");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &curlData);
+
+    qint32 ret = curl_easy_perform(curl);
+    long code;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &code);
+    qInfo("Fetching airports.csv, returned: %d\n", ret);
+    if (ret != CURLE_OK || code >= 400) {
+        QMessageBox::critical(NULL, "Error loading airports",
+                              "Could not download airports.csv!",
+                              QMessageBox::Ok, QMessageBox::NoButton);
+        exit(1);
+    }
+    curl_easy_cleanup(curl);
+
+    QFile dataFile("airports.csv");
+    if (dataFile.open(QFile::WriteOnly | QFile::Truncate)) {
+        dataFile.write(curlData);
+        dataFile.close();
+    }
+
+    QTextStream in(curlData);
+    QString line, icao;
+    double lat, lon;
+    QStringList parts;
+    int count = 0;
+    while (in.readLineInto(&line)) {
+        parts = line.trimmed().split(',');
+        icao = parts.takeFirst();
+        lat = parts.takeFirst().toDouble();
+        lon = parts.takeFirst().toDouble();
+        //qDebug("[Airports] Loaded %s: %f,%f", icao.toUtf8().data(), lat, lon);
+        airports.insert(icao, QPair<double,double>(lat,lon));
+        count++;
+    }
+    qInfo("Loaded %d airports from HTTP", count);
 }
